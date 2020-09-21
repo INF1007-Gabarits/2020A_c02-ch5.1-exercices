69fb8d06d952dd28ea71473af5c55164bb1f184a
Charles Hosson <charles.hosson@polymtl.ca>
2020-09-21 11:47:10 -0400

    Removed answers.

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -3,57 +3,25 @@
 
 import random
 
-def is_even_len(string):
-	return len(string) % 2 == 0
+def get_bill(name, data):
+	INDEX_NAME = 0
+	INDEX_QUANTITY = 1
+	INDEX_PRICE = 2
+
+	return ""
 
 
-def get_num_char(string, char):
-	num_char = 0
-	for c in string:
-		num_char += 1 if c == char else 0
-	return num_char
+def format_number(number, num_decimal_digits):
+	return ""
 
-
-def get_first_part_of_name(name):
-	# Extraire le premier prénom
-	first_part = name.split("-")[0]
-	# Metre en majuscule la première lettre
-	capitalized = first_part[0].upper() + first_part[1:].lower()
-	# Faire 'Bonjour, <leNom>'
-	return "Bonjour, " + capitalized
-
-
-def get_random_sentence(animals, adjectives, fruits):
-	basic_sentence = "Aujourd’hui, j’ai vu un %s s’emparer d’un panier %s plein de %s."
-
-	# Approche avec répétition de code
-	#animal_word = animals[random.randrange(0, len(animals))]
-	#adject_word = adjectives[random.randrange(0, len(adjectives))]
-	#fruits_word = fruits[random.randrange(0, len(fruits))]
-
-	# Approche sans répétition
-	words = []
-	for word_set in (animals, adjectives, fruits):
-		words += [word_set[random.randrange(0, len(word_set))]]
-
-	# Approche bin cool sur une ligne
-	#words = [word_set[random.randrange(0, len(word_set))] for word_set in (animals, adjectives, fruits)]
-
-	return basic_sentence % tuple(words)
+def get_triangle(num_rows):
+	return ""
 
 
 if __name__ == "__main__":
-	spam = "Bonjour!"
-	parity = "pair" if is_even_len(spam) else "impair"
-	print(f"Le nombre de caractère dans la chaine '{spam}' est {parity}.")
+	print(get_bill("Äpik Gämmör", [("chaise", 1, 399.99), ("g-fuel", 69, 35.99)]))
 
-	eggs = "Hello, world!"
-	print(f"Le nombre d'occurrence de l dans '{eggs}' est : {get_num_char(eggs, 'l')}.")
+	print(format_number(-12345.678, 2))
 
-	parrot = "jEaN-MARC"
-	print(f"Pour {parrot}, on a '{get_first_part_of_name(parrot)}'.")
-
-	animals = ("chevreuil", "chien", "pigeon")
-	adjectives = ("rouge", "officiel", "lourd")
-	fruits = ("pommes", "kiwis", "mangue")
-	print(get_random_sentence(animals, adjectives, fruits))
+	print(get_triangle(2))
+	print(get_triangle(5))
--- exercice.py
+++ exercice.py
@@ -8,47 +8,14 @@ def get_bill(name, data):
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
 
-	ALIGNMENT = 10
-	TAX_RATE = 0.15
-
-	sum = 0.0
-	for item in data:
-		cost = item[INDEX_QUANTITY] * item[INDEX_PRICE]
-		sum += cost
-	taxes = sum * TAX_RATE
-	total = sum + taxes
-	return f"{name}" "\n" \
-	       f"SOUS TOTAL {sum : >{ALIGNMENT}.2f} $" "\n" \
-	       f"TAXES      {taxes : >{ALIGNMENT}.2f} $" "\n" \
-	       f"TOTAL      {total : >{ALIGNMENT}.2f} $" "\n"
+	return ""
 
 
 def format_number(number, num_decimal_digits):
-	decimal_part = abs(number) % 1.0
-	whole_part = int(abs(number))
-
-	result = f"{decimal_part:.{num_decimal_digits}f}"[1:]
-	while whole_part >= 1000:
-		result = f" {whole_part % 1000 :0>3}{result}"
-		whole_part //= 1000
-	result = f"{'-' if number < 0 else ''}{whole_part}{result}"
-
-	return result
+	return ""
 
 def get_triangle(num_rows):
-	border_char = "+"
-	triangle_char = "A"
-
-	triangle_width = 1 + (num_rows - 1) * 2
-	border_row = border_char * (triangle_width + 2)
-
-	result = border_row
-	for i in range(num_rows):
-		triangle_chars = triangle_char * (i * 2 + 1)
-		result += "\n" + f"{border_char}{triangle_chars :^{triangle_width}}{border_char}"
-	result += "\n" + border_row
-
-	return result
+	return ""
 
 
 if __name__ == "__main__":
